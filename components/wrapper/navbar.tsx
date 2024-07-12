7bd6bb04bb04359813c077c7be43da8b173b2285
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-12 00:26:46 -0400

    changed logo

--- components/wrapper/navbar.tsx
+++ components/wrapper/navbar.tsx
@@ -7,8 +7,7 @@ export default function NavBar() {
     return (
         <header className="fixed top-8 left-1/2 -translate-x-1/2 z-50 w-full max-w-[90%] rounded-full bg-white shadow-lg px-6 py-3 flex items-center justify-between">
             <Link href="#" className="flex items-center justify-center" prefetch={false}>
-                <MountainIcon className="size-6" />
-                <span className="sr-only">Acme Inc</span>
+                🙏
             </Link>
             <nav className="ml-auto flex gap-4 sm:gap-6">
                 <Link href="https://instagram.com/foolishforjesus" target="_blank" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
