bff3aaa887af5f369441ab4b861e42cf59df0fda
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-12 00:13:00 -0400

    Added SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import { Manrope } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
+import { Metadata } from 'next'
 
 const fontHeading = Manrope({
   subsets: ['latin'],
@@ -15,6 +16,15 @@ const fontBody = Manrope({
   display: 'swap',
   variable: '--font-body',
 })
+
+
+export const metadata: Metadata = {
+  title: 'Pray For Me',
+  description: 'Need prayer? Let us know. Want to pray for others? Scroll below.',
+  openGraph: {
+    images: ['https://utfs.io/f/c041feb8-8f74-405f-88f5-2dd4d958e664-6yut8w.png']
+  },
+}
 
 export default function RootLayout({ children }: {
   children: React.ReactNode
--- next.config.js
+++ next.config.js
@@ -3,26 +3,6 @@ const nextConfig = {
   images: {
     remotePatterns: [{
       protocol: 'https',
-      hostname: 'images.unsplash.com',
-      port: '',
-      pathname: '/**'
-    }, {
-      protocol: 'https',
-      hostname: 'seo-heist.s3.amazonaws.com',
-      port: '',
-      pathname: '/**'
-    }, {
-      protocol: 'https',
-      hostname: 'github.com',
-      port: '',
-      pathname: '/**'
-    }, {
-      protocol: 'https',
-      hostname: 'ansubkhan.com',
-      port: '',
-      pathname: '/**'
-    }, {
-      protocol: 'https',
       hostname: 'utfs.io',
       port: '',
       pathname: '/**'
