d8c2079b92b1ca20a8555223c42455ed73114ac0
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-17 02:23:08 -0400

    Moved from using supabase queries to using prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+export { prisma };
