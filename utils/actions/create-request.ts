c4162d46809a6df919931e2e5603577e8bf74fc8
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-17 10:47:03 -0400

    Forgot to fix conflicts

--- utils/actions/create-request.ts
+++ utils/actions/create-request.ts
@@ -59,10 +59,7 @@ export const createRequest = async (
       },
     }
   );
-<<<<<<< HEAD
 
-=======
->>>>>>> 179367fff83479415e227222a74f7f13bdf126d7
   if (moderation?.results?.[0]?.flagged) {
     await supabase
       .from("ModeratedRequest")
